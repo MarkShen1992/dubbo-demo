1b4f3de1e1ba9e2e4371178a9128c70755246721
Mark <18009838166@163.com>
2018-06-19 20:56:35 +0800

    add new function.

--- dubbo-user-api/src/main/java/com/dubbo/api/UserService.java
+++ dubbo-user-api/src/main/java/com/dubbo/api/UserService.java
@@ -1,9 +1,20 @@
 package com.dubbo.api;
 
+import com.dubbo.vo.UserVo;
+
 /**
  * 定义服务接口
  *
  */
 public interface UserService {
+	
 	String sayHello(String name);
+	
+	Integer insert(UserVo userVo);
+	
+	Integer update(UserVo UserVo);
+	
+	Integer delete(Long userId);
+	
+	UserVo getUserVoByUserId(Long userId);
 }
--- dubbo-user-provider/src/main/java/com/dubbo/impl/UserServiceImpl.java
+++ dubbo-user-provider/src/main/java/com/dubbo/impl/UserServiceImpl.java
@@ -1,13 +1,62 @@
 package com.dubbo.impl;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.BeanUtils;
+
 import com.dubbo.api.UserService;
+import com.dubbo.dao.UserDao;
+import com.dubbo.entity.UserEntity;
+import com.dubbo.vo.UserVo;
 
 /**
  * 服务具体实现
  */
 public class UserServiceImpl implements UserService {
+	
+	@Resource
+	private UserDao userDao;
 
 	public String sayHello(String name) {
 		return "Hello " + name;
 	}
+
+	@Override
+	public Integer insert(UserVo userVo) {
+		return userDao.insert(vo2entity(userVo));
+	}
+
+	@Override
+	public Integer update(UserVo userVo) {
+		return userDao.update(vo2entity(userVo));
+	}
+
+	@Override
+	public Integer delete(Long userId) {
+		return userDao.delete(userId);
+	}
+
+	@Override
+	public UserVo getUserVoByUserId(Long userId) {
+		return entity2vo(userDao.getUserEntityByUserId(userId));
+	}
+	
+	private UserEntity vo2entity(UserVo userVo) {
+		if (userVo == null) {
+			return null;
+		}
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(userVo, userEntity);
+		return userEntity;
+	}
+	
+	private UserVo entity2vo(UserEntity userEntity) {
+		if (userEntity == null) {
+			return null;
+		}
+		UserVo userVo = new UserVo();
+		BeanUtils.copyProperties(userEntity, userVo);
+		return userVo;
+	}
+	
 }
